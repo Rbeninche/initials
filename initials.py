96aff7590084c226675e3fa84823cd0a00f4ea68
Reginald Beninche <rbeninche@gmail.com>
2019-08-05 20:16:23 -0400

    created the program without function.

---
+++ initials.py
@@ -0,0 +1,8 @@
+name = input("Please enter your full name: ")
+
+name_list = list(name.split())
+
+for full_name in name_list:
+    for index in range(len(full_name)):
+        if index == 0:
+            print(full_name[index], end='')
